d52859d5974c795c349449292bab9ac7f0fc3db2
Eyosiyas94 <134216719+Eyosiyas94@users.noreply.github.com>
2024-07-05 18:24:30 +0300

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,28 +1,17 @@
-{
-	"name": "R2-D2",
-	"height": "96",
-	"mass": "32",
-	"hair_color": "n/a",
-	"skin_color": "white, blue",
-	"eye_color": "red",
-	"birth_year": "33BBY",
-	"gender": "n/a",
-	"homeworld": "https://swapi-api.alx-tools.com/api/planets/8/",
-	"films": [
-		"https://swapi-api.alx-tools.com/api/films/1/",
-		"https://swapi-api.alx-tools.com/api/films/2/",
-		"https://swapi-api.alx-tools.com/api/films/3/",
-		"https://swapi-api.alx-tools.com/api/films/4/",
-		"https://swapi-api.alx-tools.com/api/films/5/",
-		"https://swapi-api.alx-tools.com/api/films/6/",
-		"https://swapi-api.alx-tools.com/api/films/7/"
-	],
-	"species": [
-		"https://swapi-api.alx-tools.com/api/species/2/"
-	],
-	"vehicles": [],
-	"starships": [],
-	"created": "2014-12-10T15:11:50.376000Z",
-	"edited": "2014-12-20T21:17:50.311000Z",
-	"url": "https://swapi-api.alx-tools.com/api/people/3/"
-}
+#!/usr/bin/node
+
+const request = require('request');
+
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
+});
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
+};
